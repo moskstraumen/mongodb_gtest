d86304090778c9cdbea4651fb90bd155b6c8a5b7
moskstraumen <wjhzwy@gmail.com>
2016-12-28 14:16:25 -0500

    ENH: Use test fixture feature

--- mongocxx_testing_test.cpp
+++ mongocxx_testing_test.cpp
@@ -12,23 +12,58 @@ public:
     MOCK_METHOD1(init, std::string(const std::string addr));
 };
 
-TEST(MongoDbTest, OneLink)
+class MongoDbTesting : public testing::Test
+{
+public:
+	MongoDbMock mock;
+	dbClient *client;
+
+	void SetUp()
+	{
+		client = new dbClient(&mock);
+	}
+	void TearDown()
+	{
+		delete client;
+	}
+
+};
+
+TEST_F(MongoDbTesting, UnitTest01)
 {
     char *addr = "mongodb://localhost:27017/myproject";
-    MongoDbMock mock;
-    dbClient client(&mock);
+  //  MongoDbMock mock;
+  //  dbClient client(&mock);
     EXPECT_CALL(mock, init(::testing::_))
         .WillOnce(Return(std::string(addr)));
-    std::string uri = client.createDatabase(std::string(addr));
+    std::string uri = client->createDatabase(std::string(addr));
     EXPECT_STREQ(addr, uri.c_str());
 }
 
-TEST(MongoDbTest, URILink)
+TEST_F(MongoDbTesting, UnitTest02)
 {
 	std::string addr = "mongodb://localhost:27017/myproject";
 	mongodbToy m;
 	EXPECT_NO_THROW(m.init("mongodb://localhost:27017/myproject"));
 }
+
+//TEST(MongoDbTest, OneLink)
+//{
+//    char *addr = "mongodb://localhost:27017/myproject";
+//    MongoDbMock mock;
+//    dbClient client(&mock);
+//    EXPECT_CALL(mock, init(::testing::_))
+//        .WillOnce(Return(std::string(addr)));
+//    std::string uri = client.createDatabase(std::string(addr));
+//    EXPECT_STREQ(addr, uri.c_str());
+//}
+//
+//TEST(MongoDbTest, URILink)
+//{
+//	std::string addr = "mongodb://localhost:27017/myproject";
+//	mongodbToy m;
+//	EXPECT_NO_THROW(m.init("mongodb://localhost:27017/myproject"));
+//}
 
 int main(int argc, char *argv[])
 {

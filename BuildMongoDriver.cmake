b60049e8361a46cf12a252dae85b1fc8ea845529
jiahui <jiahui@jiahui-VirtualBox>
2017-02-02 15:16:45 -0500

    ENH: Build Boost within project

--- BuildMongoDriver.cmake
+++ BuildMongoDriver.cmake
@@ -4,24 +4,38 @@ include(ExternalProject)
 set(base "${CMAKE_BINARY_DIR}/ExternalProjects")
 set_property(DIRECTORY PROPERTY EP_BASE "${CMAKE_BINARY_DIR}/ExternalProjects")
 
+
 set(install_dir "${base}/Install")
 message("install dir : ${install_dir}" )
-set(boost "/home/jiahui/work/Simmune/mongo_gtest_build/ExternalProjects/Source/Boost")
-#
-#ExternalProject_Add(Boost
+#set(boost "/home/jiahui/work/Simmune/mongo_gtest_build/ExternalProjects/Source/Boost")
+
+set(boost "${base}/Source/Boost")
+set(BOOST_BUILD_COMMAND ./b2 -sNO_BZIP2=1 -sNO_ZLIB=1 --with-graph --with-serialization --with-program_options --with-thread --with-filesystem --with-system --with-random)
+###########################################################
+# Boost
+###########################################################
+ExternalProject_Add(Boost
 #        GIT_REPOSITORY "https://github.com/boostorg/boost.git"
 #        GIT_TAB "master"
-#          UPDATE_COMMAND ""
-#        CMAKE_GENERATOR ${gen}
-#  LOG_INSTALL FALSE
-#  CMAKE_ARGS
-#    -DCMAKE_INSTALL_PREFIX:PATH=${install_dir}/boost
-#    -DCMAKE_BUILD_TYPE:STRING=${build_type}
-#    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-#    ${cxx11_flag}
-#    -DCMAKE_SKIP_RPATH:BOOL=OFF
-#) 
-
+  URL "http://localhost:10080/resources/Boost/boost_1_60_0.tar.gz"
+          UPDATE_COMMAND ""
+   CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${BOOST_BUILD_COMMAND}
+  INSTALL_COMMAND ""
+  BUILD_IN_SOURCE 1
+) 
+SET(boostbootstrap bootstrap.sh )
+ExternalProject_Add_Step(Boost booststrap 
+  COMMAND  ./${boostbootstrap}     
+  COMMENT "bootstrapping boost"    
+  DEPENDEES download    
+  DEPENDERS configure
+  WORKING_DIRECTORY ${boost}
+  )
+  
+###########################################################
+# BSON
+###########################################################
 ExternalProject_Add(libBSON
   GIT_REPOSITORY "https://github.com/mongodb/libbson.git"
   GIT_TAG "master" 
@@ -41,6 +55,9 @@ ExternalProject_Add(libBSON
   set(BSON_LIB_flag -DBSON_LIBRARY:FILEPATH=${install_dir}/libBSON/lib/libbson-1.0.so)
 ##endif()
 ##
+###########################################################
+# Mongo C Driver
+###########################################################
 ExternalProject_Add(mongo-c-driver
   GIT_REPOSITORY "https://github.com/mongodb/mongo-c-driver.git"
   GIT_TAG "1.3.6" 
@@ -59,6 +76,9 @@ ExternalProject_Add(mongo-c-driver
     DEPENDS libBSON
 )
 
+###########################################################
+# Mongo C++ Driver
+###########################################################
 message("boost dir : ${boost}") 
 set(MONGOCXX_SOURCE_DIR "${base}/ExternalProjects/Source/mongo-cxx-driver")
 set(MONGOCXX_BINARY_DIR "${base}/ExternalProjects/Build/mongo-cxx-driver")
